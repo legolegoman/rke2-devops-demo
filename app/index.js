dbe58cfcb3862f446f22777bae8318282ebfa0bc
wongchunhung <chunha@gmail.com>
2026-04-16 15:56:29 +0800

    16 Apr 7

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('DevOps Demo v1.0 - RKE2 Deployment Successful!'));
+app.get('/', (req, res) => res.send('DevOps Demo v1.1 - RKE2 Deployment Successful!'));
 app.listen(3000, () => console.log('Server running on port 3000'));
